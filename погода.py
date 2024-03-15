5f6d4acaab4d9cf3a1f3a79d85c975a85853f308
phigtol <dimamir2311@gmail.com>
2024-03-15 20:21:56 +0300

    версия погоды 1.0.0

--- погода.py
+++ погода.py
@@ -3,9 +3,17 @@
 import requests
 from translatepy.translators.google import GoogleTranslate  
 
+import tkinter
 
 google_transate = GoogleTranslate()
 
+root = tkinter.Tk()
+root.geometry('500x500')
+
+tempVar = tkinter.StringVar()
+humidityVar = tkinter.StringVar()
+weatherVar = tkinter.StringVar()
+countryVar = tkinter.StringVar()
 
 
 
@@ -25,32 +33,53 @@ def get_weather(city , api_key):
   temp = data['main']['temp']
   humidity = data['main']['humidity']
   
-  print(f'Температура: {temp}℃🎈')
-  print(f'влажость: {humidity}%😱😨')
-  
+  tempVar.set(f'Температура: {temp}℃😀')
+  humidityVar.set(f'влажость: {humidity}%😱😨')
+
   weather = data['weather'][0]['main']
   translate_weather = google_transate.translate(weather,'ru')
-  print(f'погода: {translate_weather}🍔')
+  weatherVar.set(f'погода: {translate_weather}🍔')
+  
   country = data['sys']['country']
-  print(f'страна: {country}🍉')
+  countryVar.set(f'страна: {country}🍉')
 
+  return tempVar, humidityVar, weatherVar, countryVar
 
-
+ 
 
 def main():
   print("Здравствуйте в приложение погода!")
-  city = input("Введите название города: ")
+  city = city_entry.get()
   api_key = "3fe790c4c3f692ca9f0151dfa8b0f4df"
   
   get_weather(city, api_key)
 
-main()
+# main()
+
+label = tkinter.Label(root,cursor='dot',text='Введите город о котором хотите узнать информацию.',fg='white',bg='green',padx='500')
+label.pack()
+
+city_entry = tkinter.Entry(root,fg='green',cursor='boat')
+city_entry.pack()
+
+button = tkinter.Button(root,fg='black',cursor='boat',text='Искать',command=main)
+button.pack()
+
+temp_label = tkinter.Label(root,cursor='boat',textvariable=tempVar, fg='white',bg='black',padx='500')
+temp_label.pack()
 
 
 
+humidity_label = tkinter.Label(root,cursor='boat',textvariable=humidityVar, fg='white',bg='black',padx='500')
+humidity_label.pack()
+
+weather_label = tkinter.Label(root,cursor='boat',textvariable=weatherVar, fg='white',bg='black',padx='500')
+weather_label.pack()
+
+
+country_label = tkinter.Label(root,cursor='boat',textvariable=countryVar, fg='white',bg='black',padx='500')
+country_label.pack()
 
 
 
-
-
-
+root.mainloop()
